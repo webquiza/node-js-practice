5a42d94ff34c9f5ded6751c214237f02a6ee8a00
Carlos Urquiza <webquiza@gmail.com>
2021-09-16 22:22:57 -0700

    add functionality to connect to Treehouse's API

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# _Practice session using Node.js to run small application via your terminal. This session is part of the Node.js curriculum at [Treehouse](https://teamtreehouse.com/)_
+# _This program uses Node.js to run a small application on your terminal whichs connect to [Treehouse's](https://teamtreehouse.com/) API in order to see a user's course badge count and JavaScript points. 
 
 ## Setup Requirements
 
 * _Ensure you have [Node.js installed](https://nodejs.org/en/download/)_
 * _Clone this repository_
 * _CD into root directory_
-* _Run `node app.js` in your terminal to see application messages displayed_
+* _Run `node <file name>` in your terminal to see application messages displayed_
--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-console.log("Hello World!");
-console.error("Ooooops something went wong");
-console.dir({name: "Andrew", age: 33});
+// Problem: We need a simple way to look at a user's Treehouse badge count and JavaScript points
+
+// Solution: Use Node.js to connect to Treehouse's API to get user profile information to print out
+
+// Require https module
+const https = require('https');
+const username = "carlosurquiza";
+
+// Function to print message to console
+
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badges and ${points} points in JavaScript`;
+  console.log(message);
+}
+
+// Connect to the API URL (https://teamtreehouse.com/carlosurquiza.json)
+
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+  console.log(response.statusCode);
+
+  // Read the data
+  // Parse the data
+  // Print the data
+
+});
+
